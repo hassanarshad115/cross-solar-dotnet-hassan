ba8690eef15867f105da44247e99e52b71784136
Hassan <Hassan@HASSAN-PC>
2018-08-07 01:45:40 +0500

    Work is Done

--- CrossSolar/Startup.cs
+++ CrossSolar/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CrossSolar
 {
-    public class Startup
+    public class Startup 
     {
         public Startup(IConfiguration configuration)
         {
